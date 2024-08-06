80bb86f818743bca1408435acb3f894f887fe52d
rishabhraj1506 <rishabh.raj@flipkart.com>
2024-08-06 16:07:41 +0530

    first commit

---
+++ src/com/flipkart/demo/demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("STS project");
+	}
+
+}
